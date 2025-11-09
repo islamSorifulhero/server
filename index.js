4a535a37d188bd025e49c60deae95ed183f68306
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-11-09 07:00:32 +0600

    mongoDB connection

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-import React from 'react';
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const index = () => {
-    return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    );
-};
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-export default index;
+const port = process.env.PORT || 5000;
+
+// assignmentDB
+// 6z4QMLnDLLEiBHUA
+
+const uri = "mongodb+srv://assignmentDB:6z4QMLnDLLEiBHUA@cluster0.maurhd8.mongodb.net/?appName=Cluster0";
+
+
+// ---------- MongoDB Connection ----------
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+
+
+
+
+run().catch(console.dir);
+
+// Start server
+app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
