21920fcd26741b4a4e3415eacdd9b280197ca862
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-11-09 23:03:13 +0600

    api/issues added

--- index.js
+++ index.js
@@ -41,6 +41,12 @@ async function run() {
             res.send(result);
         });
 
+        app.get("/api/issues/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await issuesCollection.findOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
+
 
     } catch (err) {
         console.error("❌ MongoDB Error:", err);
